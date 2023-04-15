9095c0c5a3f1b35a8695e28f948b3349838aa2aa
Braian Savoretti <braian@gmail.com>
2023-04-15 06:45:29 -0300

    initial commit

---
+++ converter.js
@@ -0,0 +1,150 @@
+// Google sheet npm package
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+require('dotenv').config()
+
+// File handling package
+const fs = require('fs')
+
+// spreadsheet key is the long id in the sheets URL
+// const RESPONSES_SHEET_ID = '1x2BWpHXf1yjRC1ffC6P2-RaHhCb1g81mOpcUkVBi0sE'; // test import node
+const RESPONSES_SHEET_ID = '18luYPKfAXvdTctng458fHUrz911ZeZmlM3E00xJ7lvs' // Copia de New Framework - API Details Planner
+
+// Create a new document
+const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID)
+
+// Credentials for the service account
+const CREDENTIALS = JSON.parse(fs.readFileSync('googleCredentials.json'))
+
+const getContractRoles = async () => {
+  // use service account creds
+  await doc.useServiceAccountAuth({
+    client_email: CREDENTIALS.client_email,
+    private_key: CREDENTIALS.private_key,
+  })
+
+  await doc.loadInfo()
+
+  // get Service Contract rows
+  const serviceContractsheet =
+    doc.sheetsByTitle['SC - Master (ServiceContract)']
+  const serviceContractRows = await serviceContractsheet.getRows()
+  const serviceContractData = serviceContractRows.map(row => ({
+    name: row['Name'],
+    apiFieldName: row['API Field Name'],
+    get: row['Partner Needs this Information (API GET)'],
+    put: row['Allow Partner to Push Updates to Sunrun via API'],
+  }))
+
+  // get Service Contract Event rows
+  // const serviceContractEventSheet =
+  //   doc.sheetsByTitle['SCE - Master (Service_Contract_Event__c)']
+  // const serviceContractEventRows = await serviceContractEventSheet.getRows()
+  // const serviceContractEventData = serviceContractEventRows.map(row => ({
+  //   name: row['Name'],
+  //   apiFieldName: row['API Field Name'],
+  //   get: row['Partner Needs this Information (API GET)'],
+  //   put: row['Allow Partner to Push Updates to Sunrun via API'],
+  // }))
+
+  // serviceContractData.push(...serviceContractEventData)
+
+  const partnerData = {
+    contracts: {
+      get: serviceContractData
+        .filter(row => row.get === 'Yes')
+        .map(row => row.apiFieldName),
+      put: serviceContractData
+        .filter(row => row.put === 'Yes')
+        .map(row => row.apiFieldName),
+    },
+  }
+  fs.writeFileSync('partner.json', JSON.stringify(partnerData, null, 2))
+
+  // console.log(rows.map(row => row['Name']))
+}
+
+getContractRoles()
+
+const addRow = async rows => {
+  // use service account creds
+  await doc.useServiceAccountAuth({
+    client_email: CREDENTIALS.client_email,
+    private_key: CREDENTIALS.private_key,
+  })
+
+  await doc.loadInfo()
+
+  // Index of the sheet
+  let sheet = doc.sheetsByIndex[0]
+
+  for (let index = 0; index < rows.length; index++) {
+    const row = rows[index]
+    await sheet.addRow(row)
+  }
+}
+
+let rows = [
+  {
+    email: 'email@email.com',
+    user_name: 'ramesh',
+    password: 'abcd@1234',
+  },
+  {
+    email: 'email@gmail.com',
+    user_name: 'dilip',
+    password: 'abcd@1234',
+  },
+]
+
+// addRow(rows);
+
+const updateRow = async (keyValue, oldValue, newValue) => {
+  // use service account creds
+  await doc.useServiceAccountAuth({
+    client_email: CREDENTIALS.client_email,
+    private_key: CREDENTIALS.private_key,
+  })
+
+  await doc.loadInfo()
+
+  // Index of the sheet
+  let sheet = doc.sheetsByIndex[0]
+
+  let rows = await sheet.getRows()
+
+  for (let index = 0; index < rows.length; index++) {
+    const row = rows[index]
+    if (row[keyValue] === oldValue) {
+      rows[index][keyValue] = newValue
+      await rows[index].save()
+      break
+    }
+  }
+}
+
+// updateRow('email', 'email@gmail.com', 'ramesh@ramesh.com')
+
+const deleteRow = async (keyValue, thisValue) => {
+  // use service account creds
+  await doc.useServiceAccountAuth({
+    client_email: CREDENTIALS.client_email,
+    private_key: CREDENTIALS.private_key,
+  })
+
+  await doc.loadInfo()
+
+  // Index of the sheet
+  let sheet = doc.sheetsByIndex[0]
+
+  let rows = await sheet.getRows()
+
+  for (let index = 0; index < rows.length; index++) {
+    const row = rows[index]
+    if (row[keyValue] === thisValue) {
+      await rows[index].delete()
+      break
+    }
+  }
+}
+
+// deleteRow('email', 'ramesh@ramesh.com')
